2de4b7fbfaabadddd610ba0fb6f32916f1ab194e
danielt812 <danielt812@gmail.com>
2018-08-10 22:22:58 -0500

    heroku fix

---
+++ models/note.js
@@ -0,0 +1,18 @@
+//Dependencies
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var NoteSchema = new Schema({
+    //Note
+    title: {
+        type: String
+    },
+    body: {
+        type: String
+    }
+});
+
+var Note = mongoose.model("Note", NoteSchema);
+
+module.exports = Note;
